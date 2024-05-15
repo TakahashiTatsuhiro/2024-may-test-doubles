f97240b64784198ae6006c1a7a7486968515d4af
Gordon Krull <gordon.krull@broadcom.com>
2024-05-15 17:14:42 +0900

    Initial commit

---
+++ src/test/kotlin/rocket/LauncherTest.kt
@@ -0,0 +1,12 @@
+package rocket
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class LauncherTest {
+
+    @Test
+    fun wiringTest() {
+        assertTrue(true)
+    }
+}
