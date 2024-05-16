b05512627b6b4ec71bdb565e99f9d2356db40a5a
Gordon Krull <gordon.krull@broadcom.com>
2024-05-16 11:03:27 +0900

    Incorrectly passing Dummy

--- src/main/kotlin/rocket/Launcher.kt
+++ src/main/kotlin/rocket/Launcher.kt
@@ -3,7 +3,11 @@ package rocket
 class Launcher {
     companion object {
         fun launchRocket(rocket: Rocket, launchCode: LaunchCode) {
+            try {
+                rocket.launch()
+            } catch (e: Exception) {
 
+            }
         }
     }
 }
